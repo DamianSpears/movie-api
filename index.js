e5c8fe08e61b06f9c7a0345aac9d90bfc4a84b6f
Damian Spears <ryanspears97@gmail.com>
2023-09-09 13:37:26 -0400

    Updated CORS to allow new origin

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ const Users = Models.User;
 
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixproject.netlify.app']
+app.use(cors({
+   origin: (origin, callback) => {
+     if (!origin) return callback(null, true);
+     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+       let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+       return callback(new Error(message), false);
+     }
+     return callback(null, true);
+   }
+ }));
 
 //mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true }); //links external database to index.js files using Mongoose
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
